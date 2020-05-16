55d627aa42de41883849a676029a6f2d78716fd2
Sagara Idriss <sagara.idriss@gmail.com>
2020-05-16 15:32:57 +0200

    [f] affichage des details du cour 2

---
+++ frontend/src/app/Components/list-cours/detail-cour/detail-cour.component.ts
@@ -0,0 +1,29 @@
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {CoursService} from "../../../Service/cours.service";
+import {Cours} from "../../../Interface/cours";
+
+@Component({
+  selector: 'app-detail-cour',
+  templateUrl: './detail-cour.component.html',
+  styleUrls: ['./detail-cour.component.scss']
+})
+export class DetailCourComponent implements OnInit {
+  cours: Cours
+
+  constructor(private route: ActivatedRoute, private  coursService: CoursService) {
+  }
+
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.getCoursByIdProf(params['id']); //  recuperation de l'id passé par l'url
+    });
+  }
+
+  getCoursByIdProf(id: string) {
+    this.coursService.getCoursByProfeseurID(id).then(res => {
+      this.cours = res;
+    });
+  }
+
+}
