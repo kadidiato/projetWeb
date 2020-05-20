5aa99f3cd73e302a0d061f99fe4fbcab2023933f
cashenvic <comptes.kh@protonmail.com>
2020-05-20 20:31:12 +0200

    gestion connexion prof++ -- in progress

--- frontend/src/app/Components/sign-in/sign-in.component.ts
+++ frontend/src/app/Components/sign-in/sign-in.component.ts
@@ -19,6 +19,7 @@ export class SignInComponent implements OnInit {
   signInForm: FormGroup;
   errorMessage: any;
   isAuth: boolean;
+  type: string;
 
   constructor(private afAuth: AngularFireAuth, private authService: AuthService,
               private profService: ProfService, private elevesService: ElevesService,
@@ -43,10 +44,11 @@ export class SignInComponent implements OnInit {
    * login with Google acompt
    */
   loginGoogle() {
+    this.type = this.signInForm.get('type').value;
     this.afAuth.auth.signInWithPopup(new auth.GoogleAuthProvider()).then(
       u => {
         this.route.navigate(['/profil']);
-        this.sendToServeur();
+        this.sendToServeur(this.type);
         /*this.message.add({
           severity: 'success',
           summary: `Bienvenue ${u.user.displayName}`,
@@ -66,6 +68,7 @@ export class SignInComponent implements OnInit {
   // formGroup pour la connexion avec email et le passwod
   initForm() {
     this.signInForm = this.formBuilder.group({
+      type: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.pattern(/[0-9a-zA-Z]{6,}/)]]
     });
@@ -77,10 +80,10 @@ export class SignInComponent implements OnInit {
   onSubmit() {
     const email = this.signInForm.get('email').value;
     const password = this.signInForm.get('password').value;
+    this.type = this.signInForm.get('type').value;
     this.afAuth.auth.signInWithEmailAndPassword(email, password).then(
       u => {
-        this.route.navigate(['/profil']);
-        this.sendToServeur();
+        this.sendToServeur(this.type);
         /*this.message.add({severity:'success',
           summary:`Bienvenue `,
           detail:'Vous pouvez commander vos films et plats 😁!'});
@@ -98,10 +101,13 @@ export class SignInComponent implements OnInit {
   /**
    * envoie les information du client au serveur il s'atend a recevoir (email qui est obligatoir)
    */
-  sendToServeur() {
+  sendToServeur(type) {
+    console.log('type');
+    console.log(type);
     this.afAuth.user.subscribe(eleve => {
+      localStorage.setItem('type', this.type);
       const i = eleve.displayName.indexOf(' '); // couper en 2 displayname pour avoir le prenom et le nom
-      if (eleve.uid) {
+      if (type === 'eleve') {
         this.elevesService.addEleve({
           // variable que le serveur s'attend a recevoir
           nomEleve: eleve.displayName.substr(0, i),
@@ -110,7 +116,10 @@ export class SignInComponent implements OnInit {
           photo: eleve.photoURL,
           uid: eleve.uid,
         }).then(data => {
+          this.type = '';
         });
+
+        this.route.navigate(['/profil']);
       } else {
         this.profService.addProf({
           // variable que le serveur s'attend a recevoir
@@ -120,7 +129,10 @@ export class SignInComponent implements OnInit {
           photo: eleve.photoURL,
           uid: eleve.uid,
         }).then(data => {
+          this.type = '';
         });
+
+        this.route.navigate(['/profil']);
       }
     });
   }
