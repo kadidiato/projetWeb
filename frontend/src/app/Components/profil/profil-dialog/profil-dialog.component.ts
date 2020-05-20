b9087f9bbde9ee2d689fb2ab42f8473ff465fcd2
Sagara Idriss <sagara.idriss@gmail.com>
2020-05-20 14:10:58 +0200

    ADD pop profil

---
+++ frontend/src/app/Components/profil/profil-dialog/profil-dialog.component.ts
@@ -0,0 +1,48 @@
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Cours} from "../../../Interface/cours";
+import {Eleves} from "../../../Interface/eleve";
+import {ElevesService} from "../../../Service/eleves.service";
+
+@Component({
+  selector: 'app-profil-dialog',
+  templateUrl: './profil-dialog.component.html',
+  styleUrls: ['./profil-dialog.component.scss']
+})
+export class ProfilDialogComponent implements OnInit {
+
+  /**
+   * reference vers l'utilisateur
+   */
+  @Input() eleve: Eleves;
+
+  /**
+   * boolean pour afficher le dialog
+   */
+  @Input() afficherDialog = false;
+
+  /**
+   * evenement après la fermeture du dialog
+   */
+  @Output() onDialogHide = new EventEmitter(true);
+
+  constructor(private elevesService: ElevesService) {
+  }
+
+  ngOnInit(): void {
+  }
+
+  onHide(): void {
+    this.onDialogHide.emit();
+  }
+
+  ajouterEleve() {
+    this.elevesService.updateEleve(this.eleve).subscribe((response) => {
+      this.eleve = response;
+      this.afficherDialog = false;
+      console.log(this.eleve);
+    }, error => {
+      console.log(error);
+    });
+
+  }
+}
